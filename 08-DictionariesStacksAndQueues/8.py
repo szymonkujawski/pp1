c2964c4c7a594b210c3dfa282bcb423dc5c4f5f6
szymonkujawski <146941918+szymonkujawski@users.noreply.github.com>
2023-11-29 12:08:07 +0100

    Add files via upload

---
+++ 08-DictionariesStacksAndQueues/8.py
@@ -0,0 +1,11 @@
+mobile = {
+    "OS":"IOS",
+    "manufacturer":"Apple",
+    "Model":"IPhone",
+    "Model_Number":14,
+    "Storage_in_GB":256,
+    "Color":"Space Grey"
+}
+
+for key,value in mobile.items():
+    print(f"{key} : {value}")
